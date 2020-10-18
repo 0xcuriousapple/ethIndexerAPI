2674c0f748b77de5ec016709aa64acf2cc4819bb
abhishekvispute <abhivispute33@gmail.com>
2020-10-18 21:54:50 +0530

    Final Test

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -18,18 +18,18 @@ let ecount = 0;
 // Some requests may get ECONNRESET, depending on on our call freqency and connection
 // For them, we are doing one by one, by putting good amuount of time between each API call.
 
-async function retry(blockNumber) {
+async function retry(blockNumber, web3) {
     setTimeout(() => {
         console.log(`Requesting block ${blockNumber} again`)
         web3.eth.getBlock(blockNumber, function (err, block) {
             if (err) {
                 console.log(`Repeat Error for ${blockNumber}`)
-                retry(blockNumber);
+                retry(blockNumber, web3);
                 //may lead to to infinite loop, better condition is needed, we will see it later, for now, lets assume user notice this from console statement
             }
-            if (res) {
+            if (block) {
                 console.log(` block ${blockNumber} received`)
-                res.transactions.forEach(update);
+                block.transactions.forEach(update);
                 count++;
             }
         })
@@ -43,9 +43,13 @@ async function update(item) {
 
     console.log(`Update Called for Tx ${item.hash} Block ${item.blockNumber}`);
     let x = 0;
+
+    // Ideally this increment should be in callback of db call, but as you can see I had it there, but there is slight provlem with mongoose, it doesnt return on creation call so we have to do it here
+    ucount = ucount + 2;
+
     usertoTx.findOneAndUpdate({ address: item.from }, { $push: { tx: item.hash } }, { upsert: true }, function (err, res) {
         if (res) {
-            ucount++;
+            // ucount++;
             console.log(`update from for tx ${item.hash}`)
             // console.log(`Entry for user ${item.from} updated`)
         }
@@ -57,7 +61,7 @@ async function update(item) {
 
     usertoTx.findOneAndUpdate({ address: item.to }, { $push: { tx: item.hash } }, { upsert: true }, function (err, res) {
         if (res) {
-            ucount++;
+            // ucount++;
             console.log(`update to for tx ${item.hash}`)
             //console.log(`Entry for user ${item.from} updated`)
         }
@@ -116,7 +120,9 @@ module.exports = {
 
     },
 
-    //this function is to update database to latest block
+
+    // Additional Feature : To update the database from previous update to latest block
+    // not implemented
     updateDatabase: function (req, res) {
 
 
@@ -125,56 +131,101 @@ module.exports = {
     },
 
     test: async function (req, res) {
-        console.log('asasd');
-        usertoTx.findOneAndUpdate({ address: "qqq" }, { $push: { tx: "dfg" } }, { upsert: true }, function (err, res) {
-            console.log(res);
-        });
-    },
-    genesis: async function (req, res) {
-
-
+        // console.log('asasd');
+        // usertoTx.findOneAndUpdate({ address: "qqq" }, { $push: { tx: "dfg" } }, { upsert: true }, function (err, res) {
+        //     console.log(res);
+        // });
         const provider = new Web3.providers.HttpProvider(
             "https://kovan.infura.io/v3/5c50a42c0c204dbcb9d61d538046a36c"
         );
-        const web3 = new Web3(provider);
+        const provider1 = new Web3.providers.HttpProvider(
+            "https://kovan.infura.io/v3/7892f1957ff241e9915ee1cda089f9a3"
+        );
+        const provider2 = new Web3.providers.HttpProvider(
+            "https://kovan.infura.io/v3/b3b26e89fbfe4bd9b9fc498d5796dd3e"
+        );
+
+
+        let web3Arr = {}
+        web3Arr[0] = new Web3(provider);
+        web3Arr[1] = new Web3(provider1);
+        web3Arr[2] = new Web3(provider2);
+
+        retry(21594237, web3Arr[2]);
+    },
+
+    getAllTx: function (req, res) {
+
+        Tx.find({ temp: null }, function (error, result) {
+            res.json(result);
+        })
+    },
+    getAllUsers: function (req, res) {
+        usertoTx.find({ temp: null }, function (error, result) {
+            res.json(result);
+        })
+    },
+    genesis: async function (req, res) {
+
+        // Multiple endpoints for load balancing
+        const provider = new Web3.providers.HttpProvider(
+            "https://kovan.infura.io/v3/5c50a42c0c204dbcb9d61d538046a36c"
+        );
+        const provider1 = new Web3.providers.HttpProvider(
+            "https://kovan.infura.io/v3/7892f1957ff241e9915ee1cda089f9a3"
+        );
+        const provider2 = new Web3.providers.HttpProvider(
+            "https://kovan.infura.io/v3/b3b26e89fbfe4bd9b9fc498d5796dd3e"
+        );
+
+
+        let web3Arr = {}
+        web3Arr[0] = new Web3(provider);
+        web3Arr[1] = new Web3(provider1);
+        web3Arr[2] = new Web3(provider2);
 
         // Get Latest Block Number
-        const latest = await web3.eth.getBlockNumber()
+        const latest = await web3Arr[0].eth.getBlockNumber()
 
         // Doing 10000 req in Batch of 100
 
-        let batch = new web3.eth.BatchRequest()
+        let batch = new web3Arr[0].eth.BatchRequest()
         let j = 0;
 
-        for (var i = latest; i > latest - 10; i--) {
-            console.log(`request for blockno ${i} made, ${i - latest + 9999} remaining`)
+        let k = 0;
+        for (var i = latest; i > latest - 5; i--) {
+            //console.log(`request for blockno ${i} made, ${i - latest + 9999} remaining`)
+            // console.log(k);
+
             batch.add(
-                web3.eth.getBlock.request(i, true, (err, res) => {
+                web3Arr[k % 3].eth.getBlock.request(i, true, (err, res) => {
                     if (res) {
                         ecount = ecount + res.transactions.length * 3;
                         res.transactions.forEach(update);
                         count++;
                     }
                     else {
-                        retry(i);
+                        retry(i, web3Arr[k % 3]);
                     }
                 })
             )
             j++;
-            if (j % 100 == 0) {
+            if (j % 10 == 0) {
                 j = 0;
                 batch.execute();
-                batch = new web3.eth.BatchRequest()
-                //console.log('Batch of 100 requested')
+                batch = new web3Arr[k % 3].eth.BatchRequest()
+                // console.log(k);
+                k++;
             }
+
 
         }
 
         batch.execute()
 
         setInterval(() => {
-            console.log(`Blocks Received : ${count} | Successful Updates in DB : ${ucount} | Expected Updates in DB : ${ecount} | Indexing ${latest} to ${latest - 9999}`)
-        }, 20000);
+            console.log(`Blocks Received : ${count} | Successful Updates : ${ucount} | Apprx. Expected Updates : ${ecount} | Total Tx : ${ecount / 3} | From ${latest} to ${latest - 9999}`)
+        }, 10000);
 
         res.json({ 'Please Check Console': 'Thanks !' })
     }
--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -1,21 +1,36 @@
 const router = require('express').Router();
 const dbController = require('../controllers/dbController');
 
+// Get user details
 router
     .route('/getdetails/:address')
     .get(dbController.getDetails)
 
+// get all tx
+router
+    .route('/getalltx')
+    .get(dbController.getAllTx)
 
+// get all users
+router
+    .route('/getallusers')
+    .get(dbController.getAllUsers)
+
+
+// Additional Feature : To update the database from previous update to latest block
+// not implemented
 router
     .route('/update')
     .get(dbController.updateDatabase)
 
-
+// To fetch latest 10k blockc
 router
     .route('/genesis')
     .get(dbController.genesis)
 
+//Testing Purpose    
 router
     .route('/test')
     .get(dbController.test)
+
 module.exports = router;
